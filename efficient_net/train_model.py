86e6ec9063e334e7c79a6ff15590aca66425d222
MChandraR <chandramadhan04@gmail.com>
2025-05-28 20:43:45 +0700

    Initial commit

---
+++ efficient_net/train_model.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+train_dir = "../datasets/train"
+val_dir = "../datasets/valid"
+test_dir = "../datasets/test"
+
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+EPOCHS = 20
+
+train_data = tf.keras.utils.image_dataset_from_directory(
+    train_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode="categorical"
+)
+
+val_data = tf.keras.utils.image_dataset_from_directory(
+    val_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode="categorical"
+)
+
+test_data = tf.keras.utils.image_dataset_from_directory(
+    test_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode="categorical",
+    shuffle=False
+)
+
+# Terapkan normalisasi
+def preprocess(ds):
+    return ds.map(lambda x, y: (preprocess_input(x), y))
+
+train_data = preprocess(train_data)
+val_data = preprocess(val_data)
+test_data = preprocess(test_data)
+
+
+base_model = EfficientNetB0(
+    include_top=False,
+    weights='imagenet',
+    input_shape=(224, 224, 3)
+)
+
+base_model.trainable = False
+# # buka jika trainable = True
+# for layer in base_model.layers[:-50]:
+#     layer.trainable = False
+
+# Tambah classifier layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.3)(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.3)(x)
+outputs = Dense(5, activation='softmax')(x)  # 5 kelas
+
+base_model.summary()
+
+model = Model(inputs=base_model.input, outputs=outputs)
+
+# Compile
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    restore_best_weights=True
+)
+
+checkpoint = ModelCheckpoint(
+    'efficientnet_mangrove_best.h5',
+    monitor='val_accuracy',
+    save_best_only=True,
+    verbose=1
+)
+
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=EPOCHS,
+    callbacks=[early_stop, checkpoint]
+)
+
+loss, acc = model.evaluate(test_data)
+print(f"\nTest Accuracy: {acc:.4f}")
+
+
+
+# Plot akurasi & loss
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Train')
+plt.plot(history.history['val_accuracy'], label='Val')
+plt.title('Akurasi')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Train')
+plt.plot(history.history['val_loss'], label='Val')
+plt.title('Loss')
+plt.legend()
+
+plt.show()
